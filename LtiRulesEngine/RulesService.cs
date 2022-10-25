d60f6ec4ea26079edc98941618c26c872c24460d
joelc <jcaples@ltisolutions.com>
2022-10-25 07:45:15 -0500

    Cleanup... started on Originations Test Context

--- LtiRulesEngine/RulesService.cs
+++ LtiRulesEngine/RulesService.cs
@@ -1,88 +1,12 @@
 using System;
-using System.Dynamic;
-using System.Text.Json;
-
 using LtiRulesEngine.dto;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
-using RulesEngine.Extensions;
 using RulesEngine.Models;
 
 namespace LtiRulesEngine {
+
     public class RulesService {
-        //public DateTime Date { get; set; }
-
-        //public int TemperatureC { get; set; }
-
-        //public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-
-        //public string? Summary { get; set; }
-
-        /*public async Task<string> Execute() {
-
-            //string jsonData = File.ReadAllText("data/workflow-01.json");
-            //var workflow = JsonSerializer.Deserialize<WorkflowDto>(jsonData);
-
-            //var workflowRules = jsonData;   //Get list of workflow rules declared in the json
-
-
-
-            //var re = new RulesEngine.RulesEngine(jsonData);
-
-            //// Declare input1,input2,input3 
-            //var input1 = new { };
-            //var input2 = new { };
-            //var input3 = new { };
-            //var resultList = await re.ExecuteAllRulesAsync("Discount", input1, input2, input3);
-
-            ////Check success for rule
-            //foreach (var result in resultList) {
-            //    Console.WriteLine($"Rule - {result.Rule.RuleName}, IsSuccess - {result.IsSuccess}");
-            //}
-
-            var workflows = new List<Workflow>() {
-                new Workflow() {
-                    WorkflowName = "Test Workflow Rule 1",
-                    Rules = new List<Rule>() {
-                        new Rule() {
-                            RuleName = "Test Rule",
-                            SuccessEvent = "Count is within tolerance.",
-                            ErrorMessage = "Over expected.",
-                            Expression = "count < 3",
-                            RuleExpressionType = RuleExpressionType.LambdaExpression
-                        }
-                    }
-                } 
-            };
-
-            var rulesEngine = new RulesEngine.RulesEngine(workflows.ToArray(), null);
-
-            dynamic datas = new ExpandoObject();
-            datas.count = 1;
-            var inputs = new dynamic[] {
-                datas
-            };
-
-            List<RuleResultTree> resultList = await rulesEngine.ExecuteAllRulesAsync("Test Workflow Rule 1", inputs);
-
-            bool outcome = false;
-
-            //Different ways to show test results:
-            outcome = resultList.TrueForAll(r => r.IsSuccess);
-
-            resultList.OnSuccess((eventName) => {
-                Console.WriteLine($"Result '{eventName}' is as expected.");
-                outcome = true;
-            });
-
-            resultList.OnFail(() => {
-                outcome = false;
-            });
-
-            Console.WriteLine($"Test outcome: {outcome}.");
-            return $"Test outcome: {outcome}.";
-
-        }*/
 
         public async Task<RulesEngineResponse> Colors(string data) {
             var result = await Colors(getDataObject(data));
---
+++ LtiRulesEngine/dto/OriginationsContext.cs
@@ -0,0 +1,7 @@
+﻿namespace LtiRulesEngine.dto {
+    public class OriginationsContext {
+        public string ContextType { get; set; } = "Undefined";
+        public bool CreditRelease { get; set; } = false;
+        public int? PctOwnership { get; set; } = null;
+    }
+}
